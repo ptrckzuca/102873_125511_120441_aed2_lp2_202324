74627d11a9988d1eccb21998e4c023187a474231
FredericoSolizGit <146753561+FredericoSolizGit@users.noreply.github.com>
2024-04-10 15:13:47 +0100

    Add files via upload

---
+++ Project_102873_125511_120441_aed2_lp2_202324/PubJournal.java
@@ -0,0 +1,63 @@
+package Project_102873_125511_120441_aed2_lp2_202324;
+
+public class PubJournal extends Publicacao {
+
+  private double jcrIF;
+  private double scopusIF;
+  private String periodicidade;
+  private int volume;
+  private int series;
+
+
+  public PubJournal(String publisher, int ano, String nome, double jcrIF, double scopusIF, String periodicidade, int volume, int series) {
+    super(publisher, ano, nome);
+    this.jcrIF = jcrIF;
+    this.scopusIF = scopusIF;
+    this.periodicidade = periodicidade;
+    this.volume = volume;
+    this.series = series;
+  }
+
+  public double getJcrIF() {
+    return jcrIF;
+  }
+
+  public void setJcrIF(double jcrIF) {
+    this.jcrIF = jcrIF;
+  }
+
+  public double getScopusIF() {
+    return scopusIF;
+  }
+
+  public void setScopusIF(double scopusIF) {
+    this.scopusIF = scopusIF;
+  }
+
+  public String getPeriodicidade() {
+    return periodicidade;
+  }
+
+  public void setPeriodicidade(String periodicidade) {
+    this.periodicidade = periodicidade;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  public int getSeries() {
+    return series;
+  }
+
+  public void setSeries(int series) {
+    this.series = series;
+  }
+
+  public void publicarJournal(Artigo artigo, int ano, String publisher) {
+  }
+}
---
+++ Project_102873_125511_120441_aed2_lp2_202324/Publicacao.java
@@ -0,0 +1,43 @@
+package Project_102873_125511_120441_aed2_lp2_202324;
+
+
+public class Publicacao {
+
+  private String publisher;
+  private int ano;
+  private String nome;
+
+  public Publicacao() {
+
+  }
+  public Publicacao(String publisher, int ano, String nome) {
+    this.publisher = publisher;
+    this.ano = ano;
+    this.nome = nome;
+  }
+
+
+  public String getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
